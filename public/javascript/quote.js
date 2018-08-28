3fde452a051c91a0f60d315a1684dd58f26b0181
arpiemaps <helloarpie@gmail.com>
2018-08-28 11:31:29 -0700

    Minor Improvements
    Styling, images and a few more goodies.

--- public/javascript/quote.js
+++ public/javascript/quote.js
@@ -46,7 +46,11 @@ $(document).ready(function() {
       }
       else {
         // give notice that fields aren't filled out
-        window.scrollTo(0, 0);
+        window.scroll({
+          top: 0, 
+          left: 0,
+          behavior: 'smooth'
+        });
         console.log('Nope');
         $("#error").show();
       }
